bc737ecbb24a7187a12f75c82d9cab3816dc1693
deepak1556 <hop2deep@gmail.com>
2018-04-13 01:50:37 +0530

    [ci skip] Define gfx::AcceleratedWidget to be uint64_t instead of NSView*
    https://chromium-review.googlesource.com/c/chromium/src/+/792295

--- atom/browser/native_window_mac.mm
+++ atom/browser/native_window_mac.mm
@@ -1526,7 +1526,7 @@ gfx::NativeWindow NativeWindowMac::GetNativeWindow() const {
 }
 
 gfx::AcceleratedWidget NativeWindowMac::GetAcceleratedWidget() const {
-  return [window_ contentView];
+  return gfx::kNullAcceleratedWidget;
 }
 
 void NativeWindowMac::SetProgressBar(double progress, const NativeWindow::ProgressState state) {
